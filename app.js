b8a2fa80f147434856d15f57bdcec24634b461d2
hasyid26 <sidsidsiddd0000@gmail.com>
2024-03-26 09:07:01 +0700

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express =require('express')
-const app =express()
-const port = 3000
+const express =require('express') //mengambil suatu fungsi dari node module
+const app =express() //memanggil fungsi express
+const port = 3000 //menentukan port
 
-app.get('/',function(req,res) {
-    res.send('hallo')
+app.get('/',function(req,res) { //untuk mengirim request dan respon kedalam web
+    res.send('hallo') //mengirim teks "hallo" kedalam web
 })
 
-app.listen(port, function () {
+app.listen(port, function () { //untuk menjalankan kode diatas dengan port yang sudah ditentukan
     console.log('konek jing')
-}) 
+})  
